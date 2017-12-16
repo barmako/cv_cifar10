6093e04097c2f04e521966b51340bfa5e96b64dd
Bar <teamagam2@gmail.com>
2017-12-16 13:32:47 +0200

    visual bag of words with pca after it. got 31.69%, highest record yet.

--- descriptors_extractor.py
+++ descriptors_extractor.py
@@ -9,13 +9,14 @@ pca = PCA(n_components=256)
 kmeans = KMeans(n_clusters=feature_size, random_state=0)
 
 kmeans_model = None
+pca_model = None
 
 
 def initialize(data):
-    global kmeans_model
-
+    global kmeans_model, pca_model
     sifts = get_sifts(data)
     kmeans_model = kmeans.fit(sifts)
+    pca_model = pca.fit([get_image_descriptor(image) for image in data])
 
 
 def get_sifts(data):
@@ -29,7 +30,7 @@ def get_sifts(data):
 
 def extract(data):
     extracted = [get_image_descriptor(image) for image in data]
-    return extracted
+    return pca_model.transform(extracted)
 
 
 def get_image_descriptor(image):
